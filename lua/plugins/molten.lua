d6c86d974bc36e8cbc72d0a6449f93f3e40250f5
mg104 <madhurgupta104@gmail.com>
2025-02-11 20:06:35 +0530

    Correct the indentation of ~/.config/nvim/lua/plugins/molten.lua a bit

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -1,24 +1,24 @@
 return {
-    {
-        "benlubas/molten-nvim",
-        version = "^1.0.0", -- Use version <2.0.0 to avoid breaking changes
-        dependencies = { "3rd/image.nvim" },
-        build = ":UpdateRemotePlugins",
-        init = function()
-            vim.g.molten_image_provider = "image.nvim"
-            vim.g.molten_output_win_max_height = 20
-        end,
-    },
-    {
-        "3rd/image.nvim",
-        opts = {
-            backend = "kitty",
-            max_width = 100,
-            max_height = 12,
-            max_height_window_percentage = math.huge,
-            max_width_window_percentage = math.huge,
-            window_overlap_clear_enabled = true,
-            window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-        },
-    }
+	{
+		"benlubas/molten-nvim",
+		version = "^1.0.0", -- Use version <2.0.0 to avoid breaking changes
+		dependencies = { "3rd/image.nvim" },
+		build = ":UpdateRemotePlugins",
+		init = function()
+			vim.g.molten_image_provider = "image.nvim"
+			vim.g.molten_output_win_max_height = 20
+		end,
+	},
+	{
+		"3rd/image.nvim",
+		opts = {
+			backend = "kitty",
+			max_width = 100,
+			max_height = 12,
+			max_height_window_percentage = math.huge,
+			max_width_window_percentage = math.huge,
+			window_overlap_clear_enabled = true,
+			window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+		},
+	}
 }
